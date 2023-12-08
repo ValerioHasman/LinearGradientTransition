b80c1bdb1d393820b421ea9ec0ec843a3606acfd
Valério Hasman <valeriopro22@gmail.com>
2023-12-08 20:09:40 -0300

    Lógica de tempo implementada, novos controles de tempo e execução.

--- GradLinear.js
+++ GradLinear.js
@@ -28,95 +28,84 @@ function executar1(btn){
 
   btn.disabled = true;
 
-  let lg = new GradienteTransicao(document.getElementById('primeiro'));
+  const lg = new GradienteTransicao(document.getElementById('primeiro'));
   lg.inicio = 'linear-gradient(360deg, rgba(0,255, 0,1) 0%, rgba(0,0,255,1) 100%)';
   lg.final =  'linear-gradient(0deg, rgba(255,0,0,1) 0%, rgba(0,255,0,1) 0%)';
-  lg.idaEVolta = false;
-  lg.ehUmLoop = false;
-  lg.tipoDeRotacao = 'ease';
   lg.executar();
   lg.esperarDeRodar(ex);
-
 }
 
 function executar2(btn){
   btn.disabled = true;
-  let lg = new GradienteTransicao(document.getElementById('segundo'));
-  lg.inicio = 'linear-gradient(45deg, rgba(26, 67, 92, 0.5) 0%, rgba(8, 142, 46, 0.5) 100%)';
-  lg.final =  'linear-gradient(45deg, rgba(8, 142, 46, 0.5) 0%, rgba(26, 67, 92, 0.5) 100%)';
+  const lg = new GradienteTransicao(document.getElementById('segundo'));
+  lg.inicio = 'linear-gradient(45deg, rgba(0, 170, 255, 0.4) 0%, rgba(170, 255, 0, 0.4) 100%)';
+  lg.final =  'linear-gradient(45deg, rgba(170, 255, 0, 0.4) 0%, rgba(0, 170, 255, 0.4) 100%)';
   lg.idaEVolta = true;
-  lg.ehUmLoop = true;
-  lg.tipoDeRotacao = 'ease';
-  lg.executar(700);
+  lg.executar();
 }
 
 function executar3(btn){
   btn.disabled = true;
-  let lg = new GradienteTransicao(document.getElementById('terceiro'));
+  const lg = new GradienteTransicao(document.getElementById('terceiro'));
   lg.inicio = 'linear-gradient(360deg, rgb(179, 62, 62) 10%, rgb(72, 193, 72) 10%, rgb(84, 255, 232) 100%)';
   lg.final =  'linear-gradient(360deg, rgb(179, 62, 62) 100%, rgb(72, 193, 72) 100%, rgb(84, 255, 232) 100%)';
-  lg.idaEVolta = false;
   lg.ehUmLoop = true;
-  lg.tipoDeRotacao = 'ease';
-  lg.executar(700);
+  lg.executar();
 }
 
 (()=>{
-  let alvo = document.getElementById('hoverDiv1');
-  let lg = new GradienteTransicao(alvo);
-  lg.tipoDeRotacao = 'ease';
-  lg.idaEVolta = true;
-  lg.ehUmLoop = false;
+  const alvo = document.getElementById('hoverDiv1');
+  const lg = new GradienteTransicao(alvo);
 
   alvo.addEventListener('mouseenter',()=>{
+    lg.interruptor = true;
+    lg.segundos = 2;
     lg.inicio = 'linear-gradient(72deg, rgba(0,255,255,0.1) 0%, rgba(0,0,255,1) 5%, rgba(0,255,255,0.1) 10%)';
     lg.final =  'linear-gradient(45deg, rgba(0,255,255,0.1) 100%, rgba(0,0,255,1) 100%, rgba(0,255,255,0.1) 100%)';
-    lg.executar(100);
+    lg.executar();
   });
   alvo.addEventListener('mouseout',()=>{
     lg.interruptor = true;
+    lg.segundos = 0;
     lg.inicio = 'linear-gradient(72deg, rgba(255,0,0,0) 0%, rgba(255,0,0,0) 100%)';
     lg.final =  'linear-gradient(72deg, rgba(255,0,0,0) 0%, rgba(255,0,0,0) 100%)';
-    lg.executar(1);
+    lg.executar();
   });
 })();
 
-
 (()=>{
-  let alvo = document.getElementById('hoverDiv2');
-  let lg = new GradienteTransicao(alvo);
-  lg.tipoDeRotacao = 'easeIn';
-  lg.idaEVolta = true;
-  lg.ehUmLoop = false;
+  const alvo = document.getElementById('hoverDiv2');
+  const lg = new GradienteTransicao(alvo);
 
   alvo.addEventListener('mouseout',()=>{
     lg.interruptor = true;
+    lg.segundos = 1;
     lg.inicio = 'linear-gradient(45deg, rgba(144,98,61,1) 0%, rgba(235,141,73,1) 100%)';
     lg.final =  'linear-gradient(45deg, rgba(144,98,61,0) 0%, rgba(235,141,73,0) 100%)';
-    lg.executar(50);
+    lg.executar();
   });
   alvo.addEventListener('mouseenter',()=>{
-    lg.inicio = 'linear-gradient(45deg, rgba(144,98,61,1) 0%, rgba(235,141,73,1) 100%)';
+    lg.interruptor = true;
+    lg.segundos = 0;
     lg.final =  'linear-gradient(45deg, rgba(144,98,61,1) 0%, rgba(235,141,73,1) 100%)';
-    lg.executar(1);
+    lg.executar();
   });
 })();
 
 (()=>{
-  let alvo = document.getElementById('hoverDiv3');
-  let lg = new GradienteTransicao(alvo);
-  lg.tipoDeRotacao = 'ease';
-  lg.idaEVolta = true;
-  lg.ehUmLoop = false;
+  const alvo = document.getElementById('hoverDiv3');
+  const lg = new GradienteTransicao(alvo);
+  lg.segundos = 1;
 
   alvo.addEventListener('mouseenter',()=>{
+    lg.interruptor = true;
     if(lg.ondeParou == ''){
       lg.inicio = 'linear-gradient(45deg, rgba(144,98,61,0) 0%, rgba(235,141,73,0) 100%)';
     } else {
       lg.inicio = lg.ondeParou;
     }
     lg.final =  'linear-gradient(45deg, rgba(144,98,61,1) 0%, rgba(235,141,73,1) 100%)';
-    lg.executar(100);
+    lg.executar();
   });
   alvo.addEventListener('mouseout',()=>{
     lg.interruptor = true;
@@ -126,11 +115,20 @@ function executar3(btn){
       lg.inicio = lg.ondeParou;
     }
     lg.final =  'linear-gradient(45deg, rgba(144,98,61,0) 0%, rgba(235,141,73,0) 100%)';
-    lg.executar(100);
+    lg.executar();
   });
 })();
 
-document.getElementById('instancia1').value = "let alvo = document.getElementById('alvo');\nlet lg = new GradienteTransicao(alvo);\nlg.inicio = 'linear-gradient(45deg, rgba(144,98,61,0) 0%, rgba(235,141,73,0) 100%)';\nlg.final = 'linear-gradient(45deg, rgba(235,141,73,1) 0%, rgba(144,98,61,1) 100%)';\nlg.tipoDeRotacao = 'ease';\nlg.idaEVolta = true;\nlg.ehUmLoop = false;\nlg.executar();\nlg.interruptor = true;"
+document.getElementById('instancia1').value = 
+`const alvo = document.getElementById('alvo');
+const lg = new GradienteTransicao(alvo);
+lg.inicio = 'linear-gradient(45deg, rgba(144,98,61,0) 0%, rgba(235,141,73,0) 100%)';
+lg.final = 'linear-gradient(276deg, rgba(235,141,73,1) 0%, rgba(144,98,61,1) 100%)';
+lg.segundos = 4;
+lg.idaEVolta = false;
+lg.ehUmLoop = false;
+lg.interruptor = false;
+lg.executar();`;
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
--- GradienteTransicao.js
+++ GradienteTransicao.js
@@ -1,17 +1,30 @@
+/**
+ * Uma classe JavaScript  que possibilita fazer transição em gradiente uma vez que o CSS não consegue fazer com transition e linear-gradient.
+ *
+ * @author Valério Luz Hasman Junior
+ * @version 2.0.0
+ */
 class GradienteTransicao {
-  #alvo = Object();
+
+  /**
+   * @type {Element}
+   */
+  #alvo;
   #inicio = String();
   #final = String();
+  #segundos = Number(4);
+  #eventTarget = new EventTarget();
   #tipoDeRotacao = String();
   #intermedio = Array();
-  #iteravel = Array();
-  #fracao = Number();
+  #funcao = null;
   #desacelera = Number();
   #iteracao = Number(500);
-  #ehUmLoop = false;
-  #interruptor = false;
-  #rodando = false;
-  #idaEVolta = false;
+  #ehUmLoop = Boolean(false) ;
+  #interruptor = Boolean(false);
+  #rodando = Boolean(false);
+  #idaEVolta = Boolean(false);
+  #esperaDeExecucao = Boolean(false);
+  #porcentagemTempo = 1;
 
   constructor(alvo = this.alvo, inicio = this.inicio, final = this.final){
     this.alvo = alvo;
@@ -20,42 +33,66 @@ class GradienteTransicao {
   }
 
   set inicio(valor){
-    this.interruptor = true;
     this.#inicio = String(valor);
   }
   set final(valor){
-    this.interruptor = true;
     this.#final = String(valor);
   }
   set alvo(valor){
-    this.#alvo = valor;
+    if(valor instanceof Element){
+      this.#alvo = valor;
+    } else {
+      throw new TypeError('alvo deve ser uma instância de Element');
+    }
   }
-  set intervalo(valor = 500){
-    this.#interruptor = true;
-    this.#fracao = Number((1 / Number(valor)).toFixed(13));
-    this.#iteracao = Number(valor);
+  set segundos(valor){
+    this.#segundos = Number(valor);
   }
   set ehUmLoop(valor){
-    this.#ehUmLoop = (valor == true);
-  }
-  set interruptor(valor){
-    this.#rodando = false;
-    this.#interruptor = (valor == true);
+    if(this.#idaEVolta){
+      console.warn('"ehUmLoop" não pode rodar porque "idaEVolta" está ligado.');
+    } else {
+      this.#ehUmLoop = Boolean(valor);
+      if(this.#ehUmLoop){
+        this.#eventTarget.addEventListener('finalizado', this.#executarLoop);
+      } else {
+        this.#eventTarget.removeEventListener('finalizado', this.#executarLoop);
+      }
+    }
   }
   set idaEVolta(valor){
-    this.#idaEVolta = (valor == true);
+    if(this.#ehUmLoop){
+      console.warn('"idaEVolta" não pode rodar porque "ehUmLoop" está ligado.');
+    } else {
+      this.#idaEVolta = Boolean(valor);
+      if(this.#idaEVolta){
+        this.#eventTarget.addEventListener('finalizado', this.#executaridaEVolta);
+      } else {
+        this.#eventTarget.removeEventListener('finalizado', this.#executaridaEVolta);
+      }
+    }
+  }
+  set interruptor(valor){
+    this.#interruptor = Boolean(valor);
   }
   set tipoDeRotacao(valor){
     this.#tipoDeRotacao = String(valor);
   }
+
   get inicio(){
     return this.#inicio;
   }
   get final(){
     return this.#final;
   }
+  get intermedio(){
+    return this.#intermedio;
+  }
   get alvo(){
     return this.#alvo;
+  }
+  get segundos(){
+    return this.#segundos;
   }
   get intervalo(){
     return this.#iteracao;
@@ -79,54 +116,93 @@ class GradienteTransicao {
     return this.recomporLG(this.#intermedio);
   }
 
-  static aguardar(ms = 1) {
+  static aguardar(ms = 0) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  desaceleracao(i = 0){
-    let metade = this.#iteracao/2;
-    switch (this.tipoDeRotacao) {
-      case 'ease':
-        return (i < metade ? this.#desacelera-- : i > this.#iteracao - metade ? ++this.#desacelera : 0)/30;
-      case 'easeIn':
-        return (i < metade ? this.#desacelera-- : 0)/30;
-      case 'easeOut':
-        return (i < metade ? 0 : ++this.#desacelera)/30;
-      default:
-        return 1;
+  #executaridaEVolta = ()=>{
+    const troca = this.#final;
+    this.#final = this.#inicio;
+    this.#inicio = troca;
+    this.executar();
+  }
+
+  #executarLoop = ()=>{
+    this.executar();
+  }
+
+  #executarDaFila = ()=>{
+    this.#esperaDeExecucao = false;
+    this.executar();
+    this.#eventTarget.removeEventListener('finalizado', this.#executarDaFila);
+  }
+
+  executar(){
+    if(!this.#rodando){
+      this.#rodando = true;
+      this.#interruptor = false;
+      this.mudaCor();
+    } else if(!this.#esperaDeExecucao) {
+      this.#esperaDeExecucao = true;
+      this.#eventTarget.addEventListener('finalizado', this.#executarDaFila);
+    } else {
     }
   }
 
-  async pintar() {
-    let volta = false;
-    this.#desacelera = this.#iteracao/2;
-    do{
-      if(!this.idaEVolta){
-        volta = false;
-        this.#intermedio = this.decomporLG(this.inicio);
-      }
-      for (let i = 0; i <= this.#iteracao && !this.interruptor; i++) {
-        this.alvo.style.background = this.recomporLG(this.#intermedio);
-        this.#intermedio.forEach((valor, index) => {
-          valor.forEach((valSub, indSub)=>{
-            if(volta){
-              this.#intermedio[index][indSub] = Number((this.#intermedio[index][indSub] + (this.#iteravel[index][indSub][1] ?
-                                                                                          -this.#iteravel[index][indSub][0] :
-                                                                                          +this.#iteravel[index][indSub][0])).toFixed(13));
-            } else {
-              this.#intermedio[index][indSub] = Number((this.#intermedio[index][indSub] + (this.#iteravel[index][indSub][1] ?
-                                                                                          +this.#iteravel[index][indSub][0] :
-                                                                                          -this.#iteravel[index][indSub][0])).toFixed(13));
-            }
-          });
+  async mudaCor(){
+    const milesegundos = this.#segundos * 1000 * this.#porcentagemTempo;
+    const comecoMilesegundos = new Date().getTime();
+    const inicio = this.decomporLG(this.#inicio);
+    const final = this.decomporLG(this.#final);
+    this.#intermedio = GradienteTransicao.clonarVetor(inicio);
+
+    for(let intervaloTempo = 0 ; intervaloTempo <= milesegundos && !this.interruptor; intervaloTempo = new Date().getTime() - comecoMilesegundos){
+
+      const porcentagemTempo = this.corrigirImprecisao( intervaloTempo / milesegundos );
+
+      this.#porcentagemTempo = porcentagemTempo;
+
+      this.alvo.style.background = this.recomporLG(this.#intermedio);
+
+      this.#intermedio.forEach((valor, index) => {
+        valor.forEach((valSub, indSub)=>{
+
+          const numeroInicial = Number(inicio[index][indSub]);
+          const numeroFinal = Number(final[index][indSub]);
+
+          let novoValor = 0;
+          let intervalo = 0;
+
+          if(numeroInicial > numeroFinal){
+            intervalo = numeroInicial - numeroFinal;
+            novoValor = this.corrigirImprecisao(numeroInicial - intervalo * porcentagemTempo);
+          } else {
+            intervalo = numeroFinal - numeroInicial;
+            novoValor = this.corrigirImprecisao(numeroInicial + intervalo * porcentagemTempo);
+          }
+          this.#intermedio[index][indSub] = novoValor;
+
         });
-        await GradienteTransicao.aguardar(this.desaceleracao(i));
+      });
+      if(!this.#interruptor){
+        await GradienteTransicao.aguardar();
       }
-      if(this.ehUmLoop){
-        volta = !volta;
-      }
-    } while (this.ehUmLoop && !this.interruptor);
+    }
     this.#rodando = false;
+    if(!this.#interruptor){
+      this.#intermedio = [];
+      this.#porcentagemTempo = 1;
+      this.alvo.style.background = this.#final;
+    }
+    this.#eventTarget.dispatchEvent(new Event('finalizado'));
+  }
+
+  /**
+   * @param {!number} numero
+   * @returns {!number}
+   */
+  corrigirImprecisao(numero = 0){
+    return Number((numero).toFixed(13));
   }
 
   decomporLG(str){
@@ -173,68 +249,44 @@ class GradienteTransicao {
     return lg;
   }
 
-  executar(intervalo = this.intervalo){
-    if(!this.#rodando){
-      this.intervalo = intervalo;
-      this.interruptor = false;
-      this.#rodando = true;
-      let inicioArr = this.decomporLG(this.inicio);
-      let finalArr = this.decomporLG(this.final);
-      this.#intermedio = inicioArr;
-      this.valorIteravelParalelo(inicioArr, finalArr);
-      this.pintar();
-    }
-  }
-
-  valorIteravelParalelo(arr1, arr2){
-
-    this.#iteravel = [];
-
-    arr1.forEach((arrSub, index) => {
-      arrSub.forEach((valor, indSub)=>{
-
-        if(this.#iteravel[index] == undefined){
-          this.#iteravel[index] = [];
-        }
-
-        this.#iteravel[index][indSub] = this.fimMaior(arr1[index][indSub], arr2[index][indSub]);
-
-      });
+  static clonarVetor(vetor = []) {
+    const novoVetor = Array();
+    vetor.forEach((dado) => {
+      if(Array.isArray(dado)){
+        novoVetor.push( GradienteTransicao.clonarVetor(dado) );
+      } else {
+        novoVetor.push(dado);
+      }
     });
+    return novoVetor;
   }
   
-  fimMaior(ini, fim){
-    if(ini > fim){
-      return [(ini-fim) * this.#fracao, false];
-    }
-    if(ini <= fim){
-      return [(fim-ini) * this.#fracao, true];
+  esperarDeRodar(funcao = Function()){
+    if(typeof funcao === typeof Function() ){
+      this.#funcao = funcao;
+      this.#eventTarget.addEventListener('finalizado', this.#rodaDaEspera);
+    } else {
+      throw new TypeError('funcao deve ser uma função.');
     }
   }
 
-  async esperarDeRodar(funcao = function(){}){
-    this.ehUmLoop = false;
-    if(this.rodando){
-      while(this.rodando && !this.interruptor){
-        await GradienteTransicao.aguardar(1);
-      }
-      if(!this.interruptor){
-        funcao();
-      }
-    }
+  #rodaDaEspera = (funcao = Function()) => {
+    this.#funcao();
+    //this.#eventTarget.removeEventListener('finalizado', this.#rodaDaEspera);
   }
-  
+
   toString(){
     return JSON.stringify(
       {
         alvo: this.alvo.id,
         inicio: this.inicio,
+        intermedio: this.recomporLG(this.#intermedio),
         final: this.final,
-        intervalo: this.intervalo,
+        segundos: this.#segundos,
         ehUmLoop: this.ehUmLoop,
         interruptor: this.interruptor,
         idaEVolta: this.idaEVolta,
-        rodando: this.rodando,
+        rodando: this.#rodando,
         tipoDeRotacao: this.tipoDeRotacao,
       },
       0,
